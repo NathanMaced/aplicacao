de632f5711b90568e8f9542835ab8c789f624383
NathanMaced <nathanmacedo02@gmail.com>
2022-08-25 19:11:58 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/aplicacao/componenteActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.aplicacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class componenteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.componente_layout);
+    }
+}
